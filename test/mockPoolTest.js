01244def3a43615ecdf23eebcc58410adc39b15b
Ede Meijer <ede.meijer88@gmail.com>
2017-02-20 15:46:00 +0100

    Extract out scriptable mocking and use the resulting storymock package

--- test/mockPoolTest.js
+++ test/mockPoolTest.js
@@ -6,14 +6,14 @@ exports.testSimpleSelect = function (test) {
     test.expect(1);
 
     const SUT = db.newMockPool()
-        .expect.connect().willSucceed()
-        .expect.query('SELECT 1 AS a').willReturn([{a: 1}])
-        .expect.close();
+        .expect('connect')
+        .expect('query', 'SELECT 1 AS a').ok([{a: 1}])
+        .expect('close');
 
     db.using(SUT.connect(), client => client.query('SELECT 1 AS a'))
         .then(res => {
             test.deepEqual(res, [{a: 1}]);
-            SUT.assertScenarioDone();
+            SUT.assertStoryDone();
             test.done();
         });
 };
@@ -22,17 +22,17 @@ exports.testTransactions = function (test) {
     test.expect(2);
 
     const SUT = db.newMockPool()
-        .expect.connect().willSucceed()
-        .expect.connect().willSucceed()
-        .expect.query(/DROP TABLE/).willSucceed()
-        .expect.query(/CREATE TABLE/).willSucceed()
-        .expect.query('BEGIN').willSucceed()
-        .expect.query('INSERT INTO test SELECT 1').willSucceed()
-        .expect.query('SELECT * FROM test').willReturn([])
-        .expect.query('COMMIT').willSucceed()
-        .expect.query('SELECT * FROM test').willReturn([{a: 1}])
-        .expect.close()
-        .expect.close();
+        .expect('connect')
+        .expect('connect')
+        .expect('query', /DROP TABLE/)
+        .expect('query', /CREATE TABLE/)
+        .expect('query', 'BEGIN')
+        .expect('query', 'INSERT INTO test SELECT 1')
+        .expect('query', 'SELECT * FROM test').ok([])
+        .expect('query', 'COMMIT')
+        .expect('query', 'SELECT * FROM test').ok([{a: 1}])
+        .expect('close')
+        .expect('close');
 
     // Testing transactional behavior by setting up 2 connections that try to read each others data
     db.using(SUT.connect(), SUT.connect(), (client1, client2) => {
@@ -49,7 +49,7 @@ exports.testTransactions = function (test) {
                 test.deepEqual([{a: 1}], data);
             })
     }).then(() => {
-        SUT.assertScenarioDone();
+        SUT.assertStoryDone();
         test.done();
     })
 };
@@ -58,28 +58,31 @@ exports.testTransactional = function (test) {
     test.expect(3);
 
     const SUT = db.newMockPool()
-        .expect.connect().willSucceed()
+        .expect('connect').ok()
 
-        .expect.query(/DROP TABLE/).willSucceed()
-        .expect.query(/CREATE TABLE/).willSucceed()
-        .expect.query(`
+        .expect('query', /DROP TABLE/)
+        .expect('query', /CREATE TABLE/)
+
+        .expect('query', `
             BEGIN
-        `).willSucceed()
-        .expect.query('INSERT INTO test SELECT 1').willSucceed()
-        .expect.query('INSERT INTO test SELECT 2').willSucceed()
-        .expect.query('COMMIT').willSucceed()
-        .expect.query('SELECT * FROM test').willReturn([{a: 1}, {a: 2}])
+        `)
+        .expect('query', 'INSERT INTO test SELECT 1')
+        .expect('query', 'INSERT INTO test SELECT 2')
+        .expect('query', 'COMMIT')
+        .expect('query', 'SELECT * FROM test').ok([{a: 1}, {a: 2}])
 
-        .expect.query(/DROP TABLE/).willSucceed()
-        .expect.query(/CREATE TABLE/).willSucceed()
-        .expect.query('BEGIN').willSucceed()
-        .expect.query('INSERT INTO test SELECT 1').willSucceed()
-        .expect.query('INSERT INTO this_table_does_not_exist SELECT 2')
-        .willRaise('relation "this_table_does_not_exist" does not exist')
-        .expect.query('ROLLBACK').willSucceed()
-        .expect.query('SELECT * FROM test').willReturn([])
+        .expect('query', /DROP TABLE/)
+        .expect('query', /CREATE TABLE/)
 
-        .expect.close();
+        .expect('query', 'BEGIN')
+
+        .expect('query', 'INSERT INTO test SELECT 1')
+        .expect('query', 'INSERT INTO this_table_does_not_exist SELECT 2')
+        .fail('relation "this_table_does_not_exist" does not exist')
+        .expect('query', 'ROLLBACK')
+        .expect('query', 'SELECT * FROM test').ok([])
+
+        .expect('close');
 
     // First we will test that two statements in a transactional block work, then we will test that when the second
     // operation fails, the first one doesn't have effect. Together, that proves atomicity.
@@ -99,7 +102,9 @@ exports.testTransactional = function (test) {
             .catch(err => test.equal('relation "this_table_does_not_exist" does not exist', err.message))
             .then(() => client.query("SELECT * FROM test"))
             .then(data => test.deepEqual([], data))
-            .then(() => test.done())
+    }).then(() => {
+        SUT.assertStoryDone();
+        test.done()
     });
 };
 
